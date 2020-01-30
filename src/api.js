5a6e7dd70514e1eed1c2cdfa8b79d7a4ba65150a
Parth Thakkar <pthakkar100@gmail.com>
2020-01-29 23:45:33 -0500

    updated api file

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ let config = {
 };
 
 async function getGamesToday() {
-  axios.get('http://data.nba.net/10s/prod/v1/20200129/scoreboard.json', config)
+  axios.get('https://data.nba.net/10s/prod/v1/20200129/scoreboard.json', config)
   .then(function (response) {
     // handle success
     console.log(response);
